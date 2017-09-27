f1fce25e4865224bb96a8a881bd8c9886e09cbca
Marfin Vladislav <marfinvlad@gmail.com>
2017-09-27 23:09:45 +0300

    fixes after mentor cheked

--- js/main.js
+++ js/main.js
@@ -1,59 +1,46 @@
+'use strict';
+
 (function () {
-  const ALT_KEYCODE = 18;
   const ARROW_RIGHT_KEYCODE = 39;
   const ARROW_LEFT_KEYCODE = 37;
 
-  const app = document.querySelector(`.app`);
-  const templates = document.querySelector(`#templates`).content;
-  const screenWelcome = templates.querySelector(`.main--welcome`);
-  const screenLevelGenre = templates.querySelector(`.main--level-genre`);
-  const screenLevelArtist = templates.querySelector(`.main--level-artist`);
-  const screenWin = templates.querySelector(`.main--result-win`);
-  const screenTimeOver = templates.querySelector(`.main--result-time-over`);
-  const screenAttemptsEnd = templates.querySelector(`.main--result-attempts-end`);
-  const screens = [screenWelcome, screenLevelGenre, screenLevelArtist, screenWin, screenTimeOver, screenAttemptsEnd];
+  const app = document.querySelector(`.js-app`);
+  const templates = document.querySelector(`.js-templates`).content;
+  const screensSelectors = [`.js-main--welcome`, `.js-main--level-genre`, `.js-main--level-artist`, `.js-main--result-win`, `.js-main--result-time-over`, `.js-main--result-attempts-end`];
 
   let oldScreen;
   let newScreen;
   let currentScreenIndex;
 
+  const getScreens = () => {
+    return screensSelectors.map((screensSelector) => templates.querySelector(screensSelector));
+  };
+
   const showScreen = (screenNumber) => {
-    oldScreen = app.querySelector(`.main`);
+    oldScreen = app.querySelector(`.js-main`);
     newScreen = screens[screenNumber];
     currentScreenIndex = screens.indexOf(newScreen);
 
     app.replaceChild(newScreen, oldScreen);
   };
 
-  const onDocumentArrowPress = (evt) => {
-    if (evt.keyCode === ARROW_RIGHT_KEYCODE) {
+  const onDocumentKeyPress = (evt) => {
+    if (evt.altKey && evt.keyCode === ARROW_RIGHT_KEYCODE) {
       if (currentScreenIndex < screens.length - 1) {
         showScreen(currentScreenIndex + 1);
       }
     }
 
-    if (evt.keyCode === ARROW_LEFT_KEYCODE) {
+    if (evt.altKey && evt.keyCode === ARROW_LEFT_KEYCODE) {
       if (currentScreenIndex > 0) {
         showScreen(currentScreenIndex - 1);
       }
     }
   };
 
-  const onDocumentKeyDown = (evt) => {
-    if (evt.keyCode === ALT_KEYCODE) {
-      document.addEventListener(`keydown`, onDocumentArrowPress);
-    }
-  };
-
-  const onDocumentKeyUp = (evt) => {
-    if (evt.keyCode === ALT_KEYCODE) {
-      document.removeEventListener(`keydown`, onDocumentArrowPress);
-    }
-  };
+  const screens = getScreens();
 
   showScreen(0);
 
-  document.addEventListener(`keydown`, onDocumentKeyDown);
-
-  document.addEventListener(`keyup`, onDocumentKeyUp);
+  document.addEventListener(`keydown`, onDocumentKeyPress);
 })();
