85a22dced052beb0d7f91349576373c9ae87651f
Marfin Vladislav <marfinvlad@gmail.com>
2017-10-20 01:30:07 +0300

    added updateScreenTimer

--- js/data/game-timer.js
+++ js/data/game-timer.js
@@ -1,8 +1,24 @@
 import Timer from '../utils/timer.js';
+import convertSecondsToMinutes from '../utils/convert-seconds-to-minutes.js';
+
+const screenTimer = document.querySelector('.js-timer-value');
+const screenTimerMinutes = screenTimer.querySelector('.js-timer-value-mins');
+const screenTimerDots = screenTimer.querySelector('.js-timer-value-dots');
+const screenTimerSeconds = screenTimer.querySelector('.js-timer-value-secs');
+
+let newTime;
 
 // Создаем таймер
 const createGameTimer = (state, maxGameTime) => {
   state.timer = new Timer(maxGameTime);
+};
+
+// Обновляем значения в верстке таймера
+const updateScreenTimer = (seconds) => {
+  newTime = convertSecondsToMinutes(seconds);
+
+  screenTimerMinutes.textContent = newTime.minutes;
+  screenTimerSeconds.textContent = newTime.seconds;
 };
 
 // Запускаем таймер
@@ -10,9 +26,12 @@ const startGameTimer = (state, minTimerDangerZone) => {
   return setInterval(() => {
 
     // Когда осталось менее 30 секунд таймер должен начать мигать красным цветом
-    if (state.timer.seconds < minTimerDangerZone) {}
+    if (state.timer.seconds < minTimerDangerZone) {
+
+    }
 
     state.timer.tick();
+    updateScreenTimer(state.timer.seconds);
   }, 1000);
 };
 
--- js/templates/screens/components-templates.js
+++ js/templates/screens/components-templates.js
@@ -13,10 +13,10 @@ const getStateTemplate = (state) => {
               class="timer-line"
               style="filter: url(.#blur); transform: rotate(-90deg) scaleY(-1); transform-origin: center"></circle>
 
-            <div class="timer-value" xmlns="http://www.w3.org/1999/xhtml">
-                <span class="timer-value-mins">${gameTime.minutes}</span><!--
-                --><span class="timer-value-dots">:</span><!--
-                --><span class="timer-value-secs">${gameTime.seconds}</span>
+            <div class="timer-value js-timer-value" xmlns="http://www.w3.org/1999/xhtml">
+                <span class="timer-value-mins js-timer-value-mins">${gameTime.minutes}</span><!--
+                --><span class="timer-value-dots js-timer-value-dots">:</span><!--
+                --><span class="timer-value-secs js-timer-value-secs">${gameTime.seconds}</span>
             </div>
           </svg>
     
